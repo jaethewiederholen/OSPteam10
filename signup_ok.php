4ca8554ee1a9ed74e384cb6f36f67185999b8c94
heejinkim908 <71063574+heejinkim908@users.noreply.github.com>
2020-12-13 13:57:50 +0900

    Update signup_ok.php

--- signup_ok.php
+++ signup_ok.php
@@ -10,7 +10,15 @@
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-
+  $sql = "SELECT * from member WHERE id ='$userid'";
+  $result = mysqli_query($conn, $sql);
+  $exist = mysqli_num_rows($result);
+  if($exist>0){
+    echo "<script> alert('이미 등록된 아이디입니다.'); </script>";
+    echo "<script> window.history.back(); </script>";
+    exit();
+  }
+  else{
   $sql = "INSERT INTO member(id, pw, gender, age, date) VALUES ('$userid', '$userpw', '$gender', '$age', '$date')";
 
   if ($conn->query($sql) === TRUE) {
@@ -22,4 +30,5 @@
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
   $conn->close();
+}
   ?>
