92e32c222f507f819ac936ac771768d70b1d9661
heejinkim908 <71063574+heejinkim908@users.noreply.github.com>
2020-12-13 13:30:50 +0900

    Update signup_ok.php

--- signup_ok.php
+++ signup_ok.php
@@ -1,32 +1,25 @@
 <?php
-
   $userid = $_POST['userid'];
-  $userpw = password_hash($_POST['userpw'], PASSWORD_DEFAULT);
+  $userpw = $_POST['userpw'];
   $gender = $_POST['sex'];
   $age = $_POST['age'];
   $date = date('Y-m-d H:i:s');
 
-  $conn = new mysqli("localhost","team10","team10","team10");
-
+  $conn = new mysqli("127.0.0.1","team10","team10","team10");
   // Check connection
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
 
-
   $sql = "INSERT INTO member(id, pw, gender, age, date) VALUES ('$userid', '$userpw', '$gender', '$age', '$date')";
-
 
   if ($conn->query($sql) === TRUE) {
     echo " <script>
     alert ('회원가입이 완료되었습니다.');
-
     location.href = 'login.php';
-
   </script>";}
   else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
-
   $conn->close();
   ?>
